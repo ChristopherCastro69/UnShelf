8e668b8ad8b30a6dd59970d1c3fe725dc68d2dc5
Topitops69 <topitops123@gmail.com>
2023-11-07 20:47:27 +0800

    Initial commit(New Project)

---
+++ app/src/main/java/com/example/unshelf/Firebase/Customer_Register.kt
@@ -0,0 +1,12 @@
+package com.example.unshelf.Firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.unshelf.R
+
+class Customer_Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_register)
+    }
+}
---
+++ app/src/main/java/com/example/unshelf/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.unshelf
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.unshelf.Firebase.Customer_Register
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val getStartedButton = findViewById<Button>(R.id.btn_get_started)
+
+        getStartedButton.setOnClickListener {
+            // Create an Intent to navigate to the Customer_Register activity
+            val intent = Intent(this, Customer_Register::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+}
