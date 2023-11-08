a9fc8bef11f19719a8c42872fa7bdae45e44f356
Topitops69 <topitops123@gmail.com>
2023-11-08 16:23:15 +0800

    Finished The log in Sign Up UI

--- app/src/main/java/com/example/unshelf/Authentication/Seller_Register.kt
+++ app/src/main/java/com/example/unshelf/Authentication/Seller_Register.kt
@@ -1,12 +1,12 @@
-package com.example.unshelf.Firebase
+package com.example.unshelf.Authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.unshelf.R
 
-class Customer_Login : AppCompatActivity() {
+class Seller_Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_customer_login)
+        setContentView(R.layout.activity_seller_register)
     }
 }
--- app/src/main/java/com/example/unshelf/MainActivity.kt
+++ app/src/main/java/com/example/unshelf/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.unshelf.Firebase.Customer_Register
+import com.example.unshelf.Authentication.Customer_Register
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
