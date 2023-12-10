1eb6a3e35a241183856d7f4ac1f38a1cbbe42f50
Terraknight <johnjosephpableo@gmail.com>
2023-12-11 03:23:49 +0800

    OrderView Firebase Connection

---
+++ app/src/main/java/com/example/unshelf/model/firestore/seller/ordersModel/ordersViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.unshelf.model.firestore.seller.ordersModel
+
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.example.unshelf.model.entities.Order
+
+class ordersViewModel{
+    val db = Firebase.firestore
+}
