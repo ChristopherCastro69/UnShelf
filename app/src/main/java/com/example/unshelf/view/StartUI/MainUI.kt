afd305bf61f3506d99c69e2a3021bd423fc4643c
Hernah <hgalosada@gmail.com>
2023-12-29 02:09:57 +0800

    changed ui for anonymous user and signed in user

--- app/src/main/java/com/example/unshelf/view/StartUI/MainUI.kt
+++ app/src/main/java/com/example/unshelf/view/StartUI/MainUI.kt
@@ -1,20 +1,24 @@
 package com.example.unshelf.view.StartUI
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.example.unshelf.R
-import com.example.unshelf.helper.UI_Tester_Helper
-import com.example.unshelf.view.authentication.Customer_Login
+import com.example.unshelf.controller.DataFetch.DataFetchRepository
 
 class MainUI : AppCompatActivity() {
+    val dataFetch = DataFetchRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnMain = findViewById<Button>(R.id.btn_get_started)
+        val btnLoading = findViewById<ProgressBar>(R.id.loading_bar)
         val spinner: Spinner = findViewById(R.id.spinnerAddress)
         val adapter = ArrayAdapter.createFromResource(
             this,
@@ -24,9 +28,16 @@ class MainUI : AppCompatActivity() {
         adapter.setDropDownViewResource(R.layout.custom_spinner_item)  // Use your custom layout for the dropdown view as well
         spinner.adapter = adapter
 
-
-
-
-        UI_Tester_Helper.UI_Test(this, btnMain, MainMarketplace::class.java);
+        btnMain.setOnClickListener(View.OnClickListener {
+            if(dataFetch.isLoading.value == false) {
+                btnMain.visibility = View.VISIBLE
+                btnLoading.visibility = View.GONE
+            } else {
+                btnMain.visibility = View.GONE
+                btnLoading.visibility = View.VISIBLE
+                val intent = Intent(this, MainMarketplace::class.java)
+                this.startActivity(intent)
+            }
+        })
     }
 }
