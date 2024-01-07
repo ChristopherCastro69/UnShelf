f166cd89a6d785231b35e55f61635393322c6937
Terraknight <johnjosephpableo@gmail.com>
2024-01-07 10:16:28 +0800

    Showcasing Order

--- app/src/main/java/com/example/unshelf/controller/OrderController.kt
+++ app/src/main/java/com/example/unshelf/controller/OrderController.kt
@@ -3,8 +3,10 @@ package com.example.unshelf.controller
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.unshelf.model.entities.Order
+import com.example.unshelf.model.entities.Seller
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObjects
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.tasks.await
@@ -24,22 +26,35 @@ class OrderController : ViewModel() {
                 var storeOrder: Order? = null
                 for(order in orderList){
                     val items = order.products
+                    Log.d("Item", "${items}")
                     for(item in items){
                         if(item.sellerID.equals(currentUser)){
                             if(storeOrder==null) {
                                 storeOrder = Order(order.checkoutID, order.paymentID,order.paymentTimestamp,order.customerID,
                                     listOf(),0.0,0.0,0.0,order.orderStatus,order.paymentMethod)
                             }
+                            Log.d("StoreOrder", "${storeOrder}")
                             storeOrder.products = storeOrder.products.plus(item)
                         }
                     }
-                    if(storeOrder != null) {
-                        storeOrders = storeOrders.plus(storeOrder)
+                   if(storeOrder != null) {
+                        Log.d("TEST", "TEST Store Order")
+                        if(storeOrders.isEmpty()){
+                            storeOrders = storeOrders.plus(storeOrder)
+                        }else{
+                            for(storeOrd in storeOrders){
+                                if(storeOrd == storeOrder){
+                                    storeOrder = null
+                                    continue
+                                }
+                            }
+                        }
                     }
                     storeOrder = null
                 }
                 _orders.value = storeOrders
             }
     }
+
 
 }
