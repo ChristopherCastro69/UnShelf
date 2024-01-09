85f6131c1a10633a3561e98814ae9dfd8586d61f
Hernah <hgalosada@gmail.com>
2024-01-09 22:35:52 +0800

    added a snapshot listener for orders

--- app/src/main/java/com/example/unshelf/view/SellerBottomNav/screens/orders/Orders.kt
+++ app/src/main/java/com/example/unshelf/view/SellerBottomNav/screens/orders/Orders.kt
@@ -35,11 +35,11 @@ import com.example.unshelf.ui.theme.PalmLeaf
 
 fun Orders(navController: NavController) {
     val orderViewModel:OrderController = viewModel()
-//    OrderController.orderList.value = emptyList()
     if(OrderController.orderList.value.isEmpty()) {
         OrderController.fetchOrder()
     }
-//    OrderController.fetchOrder()
+    //OrderController.fetchOrder()
+    val isLoading = remember { OrderController.isLoading }
     val orders = remember{ orderViewModel.orderList }
     var selectedTabIndex by remember { mutableStateOf(0) }
     val filterOptions = listOf("Pending", "Approved",  "Completed", "Cancelled", "Refunded")
@@ -77,7 +77,7 @@ fun Orders(navController: NavController) {
                     )
                 }
             }
-            if(orders.value.isNotEmpty()) {
+            if(!isLoading.value) {
                 LazyColumn {
                     items(orders.value) { order ->
                         OrderCard(order.products, order)
